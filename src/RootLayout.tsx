c3fcf40805c63041ca1128d625421dcb3daa4b8c
david-rosval <20b3010135@untels.edu.pe>
2024-09-27 04:33:26 -0500

    adding dark mode toggle and more components

---
+++ src/RootLayout.tsx
@@ -0,0 +1,17 @@
+import { ModeToggle } from "./components/mode-toggle"
+import { ThemeProvider } from "./components/theme-provider"
+
+function RootLayout( { children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="h-screen relative">
+        {children}
+        <div className="absolute bottom-0 right-0 p-2">
+          <ModeToggle />
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default RootLayout
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import RootLayout from './RootLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootLayout>
+      <App />
+    </RootLayout>
   </StrictMode>,
 )
